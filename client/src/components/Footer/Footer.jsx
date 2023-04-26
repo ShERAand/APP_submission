2e8de97ba11d9913b12c99726a1d3b27f58612a6
ShERAand <sherhnev.andrey2003@gmail.com>
2023-04-26 17:52:25 +0300

    Creating Navbar and Footer Components

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Logo from '../../img/logo.jpg'
+import '../../styles/Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" className='logo_smile'/>
+      <span>Сайт для подачи заявок в городе Калуга.</span>
+    </footer>
   )
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import '../../styles/Navbar.css'
+import Logo from '../../img/logo.jpg'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className="container">
+            <div className="logo">
+                <img src={Logo} alt="" className='logo' />
+            </div>
+            <div className="links">
+                <Link to='/?cat=road' className='Link'>Опасные дороги</Link>
+                <Link to='/?cat=road' className='Link'>Ремонт</Link>
+                <Link to='/?cat=road' className='Link'>Строительство</Link>
+                <Link to='/?cat=road' className='Link'>Коммунальные службы</Link>
+                <Link to='/?cat=road' className='Link'>Опасные места</Link>
+                <span>Андрей</span>
+                <span>Выйти</span>
+                <span className='writting'>
+                    <Link className="zayvka" to='/write'>Подать заявку</Link>
+                    </span>
+            </div>
+        </div>
+    </div>
   )
 }
 
