1d3a0b9c6d88542e203f3881980f27988d174a60
ShERAand <sherhnev.andrey2003@gmail.com>
2023-04-26 15:34:10 +0300

    Creating Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,64 @@
 import './App.css';
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider,} from "react-router-dom";
+import Register from './pages/Register'
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Single from './pages/Single';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  );
+};
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>,
+      },
+      {
+        path: "/write",
+        element: <Write/>,
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  }
+
+]);
 
 function App() {
   return (
-    <div>
-      проект
+    <div className='app'>
+      <div className='container'>
+      <RouterProvider router={router} />
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
